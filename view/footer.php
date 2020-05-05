18f47d8ccea7d9831153c53e5a43433bfb83fde4
BrianFenn <34203314+BrianFenn@users.noreply.github.com>
2020-05-04 21:38:08 -0700

    Add files via upload

---
+++ view/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p class="copyright">
+        &copy; <?php echo date("MY"); ?> Famous Quotes Archive
+    </p>
+</footer>
+</body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>Famous Quotes Archive</title>
+    <link rel="stylesheet" type="text/css"
+          href="../view/main.css">
+          <meta name="viewport" content="width=device-width, initial-scale=1">          
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>Famous Quotes Archive</h1></header>

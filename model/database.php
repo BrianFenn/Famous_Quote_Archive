90721c360bda6db4c7f431e0444e8a8b9f9817f8
BrianFenn <34203314+BrianFenn@users.noreply.github.com>
2020-05-04 21:43:35 -0700

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=famous_quotes_site';
-    $username = 'root';
-    $password = 'Kholmes1#';
+    $dsn = 'mysql:host=wm63be5w8m7gs25a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=famous_quotes_site';
+    $username = 'uhb1kcuh91da3z01';
+    $password = 'v9w6fen1mwn1rlwv';
 
     try {
         $db = new PDO($dsn, $username, $password);
